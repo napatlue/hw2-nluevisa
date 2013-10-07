3e1a03b71ad92dd6d2c9b3bf0d57f6c031cf2725
napatlue <napatlue@gmail.com>
2013-10-07 19:11:19 -0400

    final

--- hw2-nluevisa/src/main/java/edu/cmu/deiis/annotators/NameEntityAnswerScoreAnnotator.java
+++ hw2-nluevisa/src/main/java/edu/cmu/deiis/annotators/NameEntityAnswerScoreAnnotator.java
@@ -15,10 +15,8 @@ import edu.cmu.deiis.types.AnswerScore;
 import edu.cmu.deiis.types.Question;
 
 /**
- * NamedEntityScorer scores an Answer against a Question based on the % of overlapping
+ * NamedEntityScoreAnnotator scores result based on the % of overlapping of
  * NamedEntityMentions
- * 
- * @author jeff
  * 
  */
 public class NameEntityAnswerScoreAnnotator extends  AnswerScoreAnnotator {
@@ -28,24 +26,20 @@ public class NameEntityAnswerScoreAnnotator extends  AnswerScoreAnnotator {
   @Override
   public double  calculateScore(Question q, Answer ans, JCas aJCas) {
     AnnotationIndex<Annotation> namedEntities = aJCas.getAnnotationIndex(NamedEntityMention.type);
-    // get all Question NamedEntityMentions
-    Set<String> qNEs = getTokensForAnnotationSpan(q, namedEntities);
 
-    // get all Answer NamedEntityMentions
-    Set<String> aNEs = getTokensForAnnotationSpan(ans, namedEntities);
+    Set<String> qNE = getTokensForAnnotationSpan(q, namedEntities);
 
+    Set<String> aNE = getTokensForAnnotationSpan(ans, namedEntities);
 
+    double numANE = aNE.size();
 
-    double numANE = aNEs.size();
+    // find union of name entity
+    aNE.retainAll(qNE);
+    double numOverlapNEs = aNE.size();
 
-    // calculate the union of named entities
-    aNEs.retainAll(qNEs);
-    double numOverlapNEs = aNEs.size();
-
-    // divide the size of the union by the size of the answer.
+    // divide the size of union by the size of answer.
     return (numOverlapNEs / numANE);
     
-
   }
 
   /**
@@ -55,18 +49,18 @@ public class NameEntityAnswerScoreAnnotator extends  AnswerScoreAnnotator {
    *          Input Annotation span
    * @param tokens
    *          AnnotationIndex for the tokens
-   * @return
+   * @return Set of token String
    */
   private Set<String> getTokensForAnnotationSpan(Annotation annotation,
           AnnotationIndex<Annotation> namedEntities) {
-    Set<String> neSet = new HashSet<String>();
+    Set<String> NESet = new HashSet<String>();
     Iterator<Annotation> neIterator = namedEntities.subiterator(annotation);
 
     while (neIterator.hasNext()) {
       NamedEntityMention ne = (NamedEntityMention) neIterator.next();
-      neSet.add(ne.getCoveredText());
+      NESet.add(ne.getCoveredText());
     }
-    return neSet;
+    return NESet;
   }
 
 }
